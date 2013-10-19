84655c1f4d3c6cbb34f2450edccffab2b9857f33
wijaya <di_wijaya@yahoo.co.id>
2013-10-19 15:10:23 +0700

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,8 @@ class StaticPagesController < ApplicationController
   end
 
   def help
-end
+  end
+
+  def about
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,8 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include Capybara::DSL
+  
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
